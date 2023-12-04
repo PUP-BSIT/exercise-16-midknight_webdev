003c960580af65d3ad8c45e2c446260efeb6e332
Jeff Carl Monroy <jeffcmonroy@gmail.com>
2023-12-04 15:42:40 +0800

    add exercise 14

--- monroy_jeffcarl/monroy_jeffcarl.js
+++ monroy_jeffcarl/monroy_jeffcarl.js
@@ -3,91 +3,81 @@ document.addEventListener("DOMContentLoaded", function () {
   let fullNameInput = document.querySelector("#full_name");
   let commentInput = document.querySelector("#comment");
 
+  let commentsArray = [];
+
   addButton.addEventListener("click", addComment);
 
   fullNameInput.addEventListener("input", enableCommentButton);
   commentInput.addEventListener("input", enableCommentButton);
 
-  document.querySelector("#button_asc").addEventListener("click", 
-  sortCommentsAscending);
-  document.querySelector("#button_desc").addEventListener("click", 
-  sortCommentsDescending);
+  document.querySelector("#button_asc").addEventListener
+    ("click", sortCommentsAscending);
+  document.querySelector("#button_desc").addEventListener
+    ("click", sortCommentsDescending);
+
+  function enableCommentButton() {
+    let addButton = document.querySelector("#add_button");
+    addButton.disabled = fullNameInput.value.trim() === "" ||
+      commentInput.value.trim() === "";
+  }
+
+  function addComment() {
+    let fullNameInput = document.querySelector("#full_name");
+    let commentInput = document.querySelector("#comment");
+
+    let fullName = fullNameInput.value;
+    let comment = commentInput.value;
+
+    let currentDate = new Date();
+
+    let newComment = {
+      fullName: fullName,
+      comment: comment,
+      date: currentDate
+    };
+
+    commentsArray.push(newComment);
+
+    renderComments();
+
+    fullNameInput.value = "";
+    commentInput.value = "";
+    enableCommentButton();
+  }
+
+  // asc 
+  function sortCommentsAscending() {
+    commentsArray.sort((a, b) => a.date - b.date);
+    renderComments();
+  }
+
+  // desc 
+  function sortCommentsDescending() {
+    commentsArray.sort((a, b) => b.date - a.date);
+    renderComments();
+  }
+
+  function renderComments() {
+    let itemList = document.querySelector("#comment_list");
+    itemList.innerHTML = "";
+
+    commentsArray.forEach(comment => {
+      let listItem = document.createElement("li");
+      listItem.textContent = comment.fullName;
+
+      let commentText = document.createElement("p");
+      commentText.textContent = comment.comment;
+
+      let deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete";
+      deleteButton.addEventListener("click", function () {
+        commentsArray = commentsArray.filter(c => c !== comment);
+        renderComments();
+      });
+
+      listItem.appendChild(commentText);
+      listItem.appendChild(deleteButton);
+      itemList.appendChild(listItem);
+    });
+  }
 });
-
-function enableCommentButton() {
-  let fullNameInput = document.querySelector("#full_name");
-  let commentInput = document.querySelector("#comment");
-  let addButton = document.querySelector("#add_button");
-
-  addButton.disabled = fullNameInput.value.trim() === "" || 
-  commentInput.value.trim() === "";
-}
-
-function addComment() {
-  let fullNameInput = document.querySelector("#full_name");
-  let commentInput = document.querySelector("#comment");
-  let itemList = document.querySelector("#comment_list");
-
-  let fullName = fullNameInput.value;
-  let comment = commentInput.value;
-
-  let currentDate = new Date();
-
-  let newComment = {
-    fullName: fullName,
-    comment: comment,
-    date: currentDate
-  };
-
-  let listItem = document.createElement("li");
-  listItem.textContent = newComment.fullName; 
-  let commentText = document.createElement("p"); 
-  commentText.textContent = newComment.comment;
-
-  let deleteButton = document.createElement("button");
-  deleteButton.textContent = "Delete";
-  deleteButton.addEventListener("click", function () {
-    itemList.removeChild(listItem);
-  });
-  listItem.appendChild(commentText);
-  listItem.appendChild(deleteButton);
-  itemList.appendChild(listItem);
-
-  fullNameInput.value = "";
-  commentInput.value = "";
-  enableCommentButton();
-}
-
-// asc
-function sortCommentsAscending() {
-  let itemList = document.querySelector("#comment_list");
-  let comments = Array.from(itemList.children);
-
-  comments.sort((a, b) => {
-    let fullNameA = a.textContent;
-    let fullNameB = b.textContent;
-    return fullNameA.localeCompare(fullNameB);
-  });
-
-  itemList.innerHTML = "";
-  comments.forEach(comment => {
-    itemList.appendChild(comment);
-  });
-}
-
-// desc
-function sortCommentsDescending() {
-  let itemList = document.querySelector("#comment_list");
-  let comments = Array.from(itemList.children);
-
-  comments.sort((a, b) => {
-    let fullNameA = a.textContent;
-    let fullNameB = b.textContent;
-    return fullNameB.localeCompare(fullNameA);
-  });
-
-  itemList.innerHTML = "";
-  comments.forEach(comment => {
-    itemList.appendChild(comment);
-  });
-}
