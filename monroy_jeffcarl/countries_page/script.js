ead55d5b6d62f400628a0bb4c6a77f5a9897c112
Jeff Carl Monroy <jeffcmonroy@gmail.com>
2023-12-05 02:13:18 +0800

    updatesfiles and add countries page

---
+++ monroy_jeffcarl/countries_page/script.js
@@ -0,0 +1,45 @@
+function searchCountry() {
+    const input = document.getElementById('searchInput').value;
+
+    fetch(`https://restcountries.com/v3.1/name/${input}`)
+        .then(response => {
+            if (response.status === 404) {
+                throw new Error('Country not found');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const country = data[0];
+            const region = country.region;
+
+            const countryDetailsElement = document
+                .getElementById('countryDetails');
+            countryDetailsElement.innerHTML = `
+                <h2>${country.name.common}</h2>
+                <p>Capital: ${country.capital}</p>
+                <p>Region: ${region}</p>
+                <p>Population: ${country.population}</p>
+                <p>Area: ${country.area} sq km</p>
+                <p>Languages: ${Object.values(country.languages).join(', ')}</p>
+            `;
+            fetch(`https://restcountries.com/v3.1/region/${region}`)
+                .then(response => response.json())
+                .then(sameRegion => {
+                    const sameRegionElement = document
+                        .getElementById('sameRegion');
+                        sameRegionElement.innerHTML = 
+                        `<h3>Other countriesin the ${region} region:</h3>`;
+                        sameRegion.forEach(country => {
+                            sameRegionElement.innerHTML += 
+                            `<p>${country.name.common}</p>`;
+                    });
+                })
+                .catch(error => console.error('Error fetching other countries:',
+                     error));
+        })
+        .catch(error => {
+            // Country not found, show alert
+            alert('Invalid country. Please enter valid name of a country.');
+            console.error('Error fetching country details:', error);
+        });
+}
